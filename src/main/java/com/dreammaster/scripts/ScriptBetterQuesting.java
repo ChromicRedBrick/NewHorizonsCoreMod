34bef753bbd388d1863fd922165da7a156c46a87
boubou19 <miisterunknown@gmail.com>
2022-09-21 14:30:00 +0200

    change modid strings to enum for ScriptBetterQuesting

--- src/main/java/com/dreammaster/scripts/ScriptBetterQuesting.java
+++ src/main/java/com/dreammaster/scripts/ScriptBetterQuesting.java
@@ -1,5 +1,7 @@
 package com.dreammaster.scripts;
 
+import net.minecraft.item.ItemStack;
+
 import static gregtech.api.util.GT_ModHandler.getModItem;
 
 public class ScriptBetterQuesting implements IScriptLoader {
@@ -11,15 +13,18 @@ public class ScriptBetterQuesting implements IScriptLoader {
         scriptName.setLength(0);
         scriptName.append("BetterQuesting");
         dependencies.clear();
-        dependencies.add("betterquesting");
+        dependencies.add(ModIds.BetterQuesting.toString());
     }
 
     @Override
     public void loadRecipes() {
-        addShapedRecipe(getModItem("betterquesting", "submit_station", 1), new Object[] {
+        final ItemStack CHEST = getModItem("minecraft", "chest", 1);
+        final ItemStack OBJECT_SUBMISSION_STATION = getModItem("betterquesting", "submit_station", 1);
+
+        addShapedRecipe(OBJECT_SUBMISSION_STATION, new Object[] {
             "plateWood", "plateSteel", "plateWood",
             "plateSteel", "gearSteel", "plateSteel",
-            "plateWood", getModItem("minecraft", "chest", 1), "plateWood"
+            "plateWood", CHEST, "plateWood"
         });
     }
 }
